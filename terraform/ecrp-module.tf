dc28634c39fb453e617223a5bcbd3cd5ec4eaea9
Ewerton Pinheiro <ewerton.pinheiro@parfin.io>
2023-04-18 08:16:32 +0200

    novo

--- terraform/ecrp-module.tf
+++ terraform/ecrp-module.tf
@@ -1,7 +1,8 @@
-module "ecr" {
+module "public_ecr" {
   source = "terraform-aws-modules/ecr/aws"
 
   repository_name = "nodejs-hello"
+  repository_type = "public"
 
   repository_read_write_access_arns = ["arn:aws:iam::777156806021:user/githubactions", "arn:aws:iam::777156806021:role/githubactionprovider-role"]
   repository_lifecycle_policy = jsonencode({
@@ -21,6 +22,15 @@ module "ecr" {
       }
     ]
   })
+
+  public_repository_catalog_data = {
+    description       = "Docker container for some things"
+    about_text        = file("${path.module}/files/ABOUT.md")
+    usage_text        = file("${path.module}/files/USAGE.md")
+    operating_systems = ["Linux"]
+    architectures     = ["x86"]
+    logo_image_blob   = filebase64("${path.module}/files/clowd.png")
+  }
 
   tags = {
     Terraform   = "true"
--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,6 @@ terraform {
 }
 
 provider "aws" {
-  region  = "eu-central-1"
+  region  = "us-east-1"
   profile = "default"
 }
